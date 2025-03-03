baf61fac37d5ecf544c5c3daf7787c87210f865a
NthZ6K4 <131695231+NthZ6K4@users.noreply.github.com>
2025-03-03 19:43:31 +0100

    Finale V1, operationelle

--- strings.lua
+++ strings.lua
@@ -22,7 +22,7 @@ fr = {
     "[*][b]Rosporden [/b]"..lb..
     "[/list]"..lb..
     lb..
-    "A savoir qu'une version générique (sans le nom des gares) est disponible pour chaques batiments, excepté pour Plouaret Trégor."..lb..
+    "A savoir qu'une version générique (sans le nom des gares) est disponible pour chaque batiments, excepté pour Plouaret Trégor."..lb..
     "Vous les trouverez alors, soit dans les modules de gares, soit en assets."..lb..
     lb..
     lb..
@@ -42,9 +42,54 @@ fr = {
     lb..
     "[h2][b]Disclaimer :[/b][/h2]"..lb..
     "La réutilisation / publication des fichiers, que ce soit sous une forme modifiée ou inchangée (dans des repaints ou autres), nécessite l'autorisation expresse de l'auteur.\n" ..lb..
-    "L'upload de ce mod sur d'autres sites n'est pas autorisé.\n"..lb..
+    "L'upload de ce mod sur d'autres sites n'est pas autorisée.\n"..lb..
     lb..
     "[h2][b]La communauté française :[/b][/h2]"..lb..site.."",
-        },
+},
+		
+en = {
+    ["gare_ouest_etat"] = "West-State Type Station",
+    ["gare_ouest_etat_desc"] = "[h1]West-State Type Stations Pack[/h1]"..lb..
+    lb..
+    lb..
+    "In this pack, you will find several varieties of stations inspired by the former West-State company, representing Normandy, Pays de la Loire, and Brittany."..lb..
+    lb..
+    lb..
+    "[h2][b]List of available stations:[/b][/h2]"..lb..
+    "[list]"..lb..
+    "[*][b]Ancenis [/b]"..lb..
+    "[*][b]Briouze [/b]"..lb..
+    "[*][b]Cholet [/b]"..lb..
+    "[*][b]Guingamp (with and without canopy) [/b]"..lb..
+    "[*][b]Montauban de Bretagne (with embossed lettering or plaque) [/b]"..lb..
+    "[*][b]Plouaret Trégor [/b]"..lb..
+    "[*][b]Rosporden [/b]"..lb..
+    "[/list]"..lb..
+    lb..
+    "Note that a generic version (without station names) is available for each building, except for Plouaret Trégor."..lb..
+    "You will find them either in the station modules or as assets."..lb..
+    lb..
+    lb..
+    "[h2][b]Authors:[/b][/h2]"..lb..
+    "[list]"..lb..
+    "[*][b]3D Models: [/b] NTH-Z6K4, Syltheron, Yoodel22"..lb..
+    "[*][b]Textures: [/b] NTH-Z6K4, Syltheron, Yoodel22"..lb..
+    "[*][b]Scripts: [/b] NTH-Z6K4, Syltheron"..lb..
+    "[/list]"..lb..
+    lb..
+    lb..
+    "[h2][b]Update logs[/b][/h2]"..lb..
+    "[list]"..lb..
+    "[*]Main 1.0: stable"..lb..
+    "[/list]"..lb..
+    lb..
+    lb..
+    "[h2][b]Disclaimer:[/b][/h2]"..lb..
+    "Reusing/publishing files, whether modified or unmodified (in repaints or otherwise), requires the express permission of the author.\n" ..lb..
+    "Uploading this mod to other sites is not allowed.\n"..lb..
+    lb..
+    "[h2][b]The French Community:[/b][/h2]"..lb..site.."",
+},
+
 }
 end
